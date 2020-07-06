3f141fae498d22e4a5c14250eee5265f2ad9d4b4
Roni <ronicse2011@gmail.com>
2020-07-07 05:57:43 +0600

    Variable length argument

---
+++ src/varargs/java/oop/VarargsDemo.java
@@ -0,0 +1,21 @@
+package varargs.java.oop;
+
+class Calc {
+    public int add(int ... n) // array {4, 5, 6, 7, 8}
+    {
+        int sum = 0;
+        for (int i : n)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
+
+public class VarargsDemo {
+    public static void main(String[] args) {
+        Calc obj = new Calc();
+        System.out.println(obj.add(4, 5, 6, 7, 8));
+    }
+}
