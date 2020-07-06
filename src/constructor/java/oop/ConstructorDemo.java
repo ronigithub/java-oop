8f54db9ab3ed818ddfa32f2b20ff621398bf8b40
Roni <ronicse2011@gmail.com>
2020-07-07 05:50:03 +0600

    Constructor overloading

---
+++ src/constructor/java/oop/ConstructorDemo.java
@@ -0,0 +1,33 @@
+package constructor.java.oop;
+
+class Calculator {
+    int num1;
+    int num2;
+
+    public Calculator() {
+        num1 = 5;
+        num2 = 6;
+        System.out.println("in Constructor");
+    }
+
+    public Calculator(int n) {
+        num1 = n;
+        num2 = n;
+    }
+
+    public Calculator(double d, int n) {
+        num1 = (int) d;
+        num2 = n;
+    }
+
+}
+
+public class ConstructorDemo {
+    public static void main(String[] args) {
+
+        Calculator obj = new Calculator(4); // Constructor
+
+        System.out.println(obj.num1);
+
+    }
+}
