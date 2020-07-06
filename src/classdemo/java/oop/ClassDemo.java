d6bfd8febeb9dec9af559a4699844699c7e24396
Roni <ronicse2011@gmail.com>
2020-07-07 05:49:28 +0600

    ClassDemo with main method

---
+++ src/classdemo/java/oop/ClassDemo.java
@@ -0,0 +1,7 @@
+package classdemo.java.oop;
+
+public class ClassDemo {
+    public static void main(String[] args) {
+        System.out.println("Class with main method for starting program");
+    }
+}
