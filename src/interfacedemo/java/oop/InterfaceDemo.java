79ec15da7301a9832d3ee9e62faaaf0459ab0c96
Roni <ronicse2011@gmail.com>
2020-07-07 07:58:19 +0600

    interface

---
+++ src/interfacedemo/java/oop/InterfaceDemo.java
@@ -0,0 +1,40 @@
+package interfacedemo.java.oop;
+
+interface Writer {
+// By default, all method of interface has public abstract keyword
+    void writer();
+}
+
+class Pen implements Writer {
+    public void writer()
+    {
+        System.out.println("I'm a Pen");
+    }
+}
+
+class Pencil implements Writer {
+    public void writer()
+    {
+        System.out.println("I'm a Pencil");
+    }
+}
+
+class Kit {
+    public void doSomething(Writer p)
+    {
+        p.writer();
+    }
+
+}
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Kit k = new Kit();
+
+        Writer p = new Pen();
+
+        Writer pc = new Pencil();
+
+        k.doSomething(p);
+    }
+}
